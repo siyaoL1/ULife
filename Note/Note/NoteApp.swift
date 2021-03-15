663555c2e9567ba367e3577ae40feee69f0587f2
Hongyang Lin <linhy@terpmail.umd.edu>
2021-03-15 18:30:52 -0400

    initial setup

---
+++ Note/Note/NoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Hongyang Lin on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
