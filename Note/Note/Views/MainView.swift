f7640a8974bbc849dfef8e4331db58c95c97f5bb
Hongyang Lin <linhy@terpmail.umd.edu>
2021-03-17 02:00:06 -0400

    slide card

--- Note/Note/Views/MainView.swift
+++ Note/Note/Views/MainView.swift
@@ -2,7 +2,25 @@
 //  MainView.swift
 //  Note
 //
-//  Created by Hongyang Lin on 3/15/21.
+//  Created by Hongyang Lin on 3/16/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 0, green: 100, blue: 50)
+            VStack {
+                Text("Yo")
+                Text("Welcome")
+            }
+        }.clipShape(RoundedRectangle(cornerRadius: 40))
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
