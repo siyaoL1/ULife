562fb83c0224ed47b5780ac27269a3e8a6a110d2
Hongyang Lin <linhy@terpmail.umd.edu>
2021-03-18 00:19:53 -0400

    updated

--- Note/Note/Views/Note/NoteList.swift
+++ Note/Note/Views/Note/NoteList.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct NoteList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
-
+    @State private var prompt: String = "Swipe up for notes"
+    
     var filteredLandmarks: [NoteType] {
         modelData.notes.filter { note in
             (!showFavoritesOnly || note.isFavorite)
@@ -20,20 +21,26 @@ struct NoteList: View {
     var body: some View {
         NavigationView {
             
-            VStack(alignment: .leading, spacing: 0) {
+            VStack(spacing: 0) {
+                RoundedRectangle(cornerRadius: 45)
+                    .foregroundColor(.gray)
+                    .frame(width: 60,height:10,alignment: .topLeading)
+                    .padding(13)
+                Text("").frame(height: 50)
                 Toggle(isOn: $showFavoritesOnly) {
                     Text("Favorites only")
-                }.padding()
+                }.padding(20)
 
                 ForEach(filteredLandmarks) { note in
                     NavigationLink(destination: NoteDetail(note: note)) {
                         NoteRow(note: note)
                     }
                 }
-                Spacer()
-            }.frame(width: 350)
-//            .navigationTitle("Notes")
-        }
+                
+            }
+            .frame(height: UIScreen.main.bounds.height, alignment: .topLeading)
+            .background(Color(red: 249/255, green: 247/255, blue: 236/255))
+        }.clipShape(RoundedRectangle(cornerRadius: 44))
     }
 }
 
