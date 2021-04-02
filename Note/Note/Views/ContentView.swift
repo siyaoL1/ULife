30be9f5877539a614896d5f4106fc012387a112f
Hongyang Lin <linhy@terpmail.umd.edu>
2021-04-01 21:52:21 -0400

    Details modified

--- Note/Note/Views/ContentView.swift
+++ Note/Note/Views/ContentView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 import CoreData
 
+struct ContentView: View {
+    @EnvironmentObject var modelData: ModelData
+    @State var showList: Bool = false
+    
+    var body: some View {
+        ZStack(alignment: Alignment.top) {
+            if modelData.showMainPanel {
+                HStack{
+                    MainPanelBar().padding(.top, -UIScreen.main.bounds.width*0.95).navigate(to: ToDoView(), when: $modelData.showCalendarPanel)
+                }
+            }
+            if modelData.showNotesPanel {
+                NotePanel()
+            }
+        }
+    }
+}
+
 extension View {
-    // Navigate to a new view.
-    // - Parameters:
-    // - view: View to navigate to.
-    // - binding: Only navigates when this condition is `true`.
     func navigate<NewView: View>(to view: NewView, when binding: Binding<Bool>) -> some View {
         NavigationView {
             ZStack {
@@ -27,24 +41,6 @@ extension View {
                 ) {
                     EmptyView()
                 }
-            }
-        }
-    }
-}
-
-struct ContentView: View {
-    @EnvironmentObject var modelData: ModelData
-    @State var showList: Bool = false
-    
-    var body: some View {
-        ZStack(alignment: Alignment.top) {
-            if modelData.showMainPanel {
-                HStack{
-                    MainPanelBar().padding(.top, -UIScreen.main.bounds.width*0.95).navigate(to: ToDoView(), when: $modelData.showCalendarPanel)
-                }
-            }
-            if modelData.showNotesPanel {
-                NotePanel()
             }
         }
     }
