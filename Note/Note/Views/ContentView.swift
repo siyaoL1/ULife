556017962d1a3db67575747b79a83dd8fe17d452
Hongyang Lin <linhy@terpmail.umd.edu>
2021-04-01 21:37:15 -0400

    menu bar modified

--- Note/Note/Views/ContentView.swift
+++ Note/Note/Views/ContentView.swift
@@ -8,38 +8,45 @@
 import SwiftUI
 import CoreData
 
+extension View {
+    // Navigate to a new view.
+    // - Parameters:
+    // - view: View to navigate to.
+    // - binding: Only navigates when this condition is `true`.
+    func navigate<NewView: View>(to view: NewView, when binding: Binding<Bool>) -> some View {
+        NavigationView {
+            ZStack {
+                self
+                    .navigationBarTitle("")
+                    .navigationBarHidden(true)
+                NavigationLink(
+                    destination: view
+                        .navigationBarTitle("")
+                        .navigationBarHidden(true),
+                    isActive: binding
+                ) {
+                    EmptyView()
+                }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     @EnvironmentObject var modelData: ModelData
     @State var showList: Bool = false
     
     var body: some View {
-        
-        if !modelData.showCalendarPanel {
-            ZStack(alignment: Alignment.top) {
-                if modelData.showMainPanel {
-                    VStack{
-                        HStack(){
-                            
-                                Button(action: {modelData.showCalendarPanel = true}){
-                                    Image(systemName: "calendar")
-                                        .font(.system(size: 25.0))
-                                }.padding()
-                            
-                            Spacer()
-                            Button(action: {}){
-                                Image(systemName: "gearshape")
-                                    .font(.system(size: 25.0))
-                            }.padding()
-                        }
-                    }
+        ZStack(alignment: Alignment.top) {
+            if modelData.showMainPanel {
+                HStack{
+                    MainPanelBar().padding(.top, -UIScreen.main.bounds.width*0.95).navigate(to: ToDoView(), when: $modelData.showCalendarPanel)
                 }
+            }
+            if modelData.showNotesPanel {
                 NotePanel()
             }
-        } else {
-            ToDoView()
         }
-            
-        
     }
 }
 
